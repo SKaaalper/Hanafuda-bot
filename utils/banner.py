7e39ab9dacadb7255c803afa2bed7aa5b0cef2bc
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-09 16:20:19 +0800

    Update banner.py

--- utils/banner.py
+++ utils/banner.py
@@ -1,6 +1,8 @@
+from colorama import Fore, Style
+
 banner = f"""
 {Fore.YELLOW}╔════════════════════════════════════════╗
-{Fore.YELLOW}║      🚀  花牌自动存款工具 🚀           ║
+{Fore.YELLOW}║      🚀  hanafuda自动工具 🚀           ║
 {Fore.YELLOW}║  👤    脚本编写：@qklxsqf              ║
 {Fore.YELLOW}║  📢  电报频道：https://t.me/ksqxszq    ║
 {Fore.YELLOW}╚════════════════════════════════════════╝{Style.RESET_ALL}
