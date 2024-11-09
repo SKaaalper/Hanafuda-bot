dca63171e8df4198fe7ed317c18263e58e2f6ae1
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-09 15:44:41 +0800

    Create banner.py

---
+++ banner.py
@@ -0,0 +1,7 @@
+banner = f"""
+{Fore.YELLOW}╔════════════════════════════════════════╗
+{Fore.YELLOW}║      🚀  花牌自动存款工具 🚀           ║
+{Fore.YELLOW}║  👤    脚本编写：@qklxsqf              ║
+{Fore.YELLOW}║  📢  电报频道：https://t.me/ksqxszq    ║
+{Fore.YELLOW}╚════════════════════════════════════════╝{Style.RESET_ALL}
+"""
