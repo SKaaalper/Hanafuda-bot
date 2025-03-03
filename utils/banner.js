83aef01c7cc1695923bc26c82e3fa275adbc8ea8
子清 <120438383+ziqing888@users.noreply.github.com>
2025-03-03 21:32:02 +0800

    Create banner.js

---
+++ utils/banner.js
@@ -0,0 +1,12 @@
+
+module.exports = function printBanner(chalk, printMessage) {
+  const banner = `
+${chalk.yellow('╔════════════════════════════════════════╗')}
+${chalk.yellow('║      🚀  hanafuda自动工具 🚀           ║')}
+${chalk.yellow('║  👤    脚本编写：@qklxsqf              ║')}
+${chalk.yellow('║  📢  电报频道：https://t.me/ksqxszq    ║')}
+${chalk.yellow('╚════════════════════════════════════════╝')}${chalk.reset('')}
+  `;
+  console.log(banner);
+  printMessage('请确保 tokensgrow.json 和 pvkey.txt 已准备好！', 'info');
+};
