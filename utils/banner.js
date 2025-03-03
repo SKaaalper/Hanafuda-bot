ff93eb69d8f6051c5a4db202d27716d730a02705
子清 <120438383+ziqing888@users.noreply.github.com>
2025-03-03 19:55:12 +0800

    Update banner.js

--- utils/banner.js
+++ utils/banner.js
@@ -1,9 +1,12 @@
-from colorama import Fore, Style
 
-banner = f"""
-{Fore.YELLOW}╔════════════════════════════════════════╗
-{Fore.YELLOW}║      🚀  hanafuda自动工具 🚀           ║
-{Fore.YELLOW}║  👤    脚本编写：@qklxsqf              ║
-{Fore.YELLOW}║  📢  电报频道：https://t.me/ksqxszq    ║
-{Fore.YELLOW}╚════════════════════════════════════════╝{Style.RESET_ALL}
-"""
+module.exports = function printBanner(chalk, printMessage) {
+  const banner = `
+${chalk.yellow('╔════════════════════════════════════════╗')}
+${chalk.yellow('║      🚀  hanafuda自动工具 🚀           ║')}
+${chalk.yellow('║  👤    脚本编写：@qklxsqf              ║')}
+${chalk.yellow('║  📢  电报频道：https://t.me/ksqxszq    ║')}
+${chalk.yellow('╚════════════════════════════════════════╝')}${chalk.reset('')}
+  `;
+  console.log(banner);
+  printMessage('请确保 tokensgrow.json 和 pvkey.txt 已准备好！', 'info');
+};
