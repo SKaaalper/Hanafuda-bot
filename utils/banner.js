8aa32c5281f19238536fbb03c86f534c6bc8d328
_skaaalper <159729426+SKaaalper@users.noreply.github.com>
2025-03-08 21:55:38 +0800

    Update banner.js

--- utils/banner.js
+++ utils/banner.js
@@ -1,12 +1,13 @@
-
-module.exports = function printBanner(chalk, printMessage) {
-  const banner = `
-${chalk.yellow('╔════════════════════════════════════════╗')}
-${chalk.yellow('║      🚀  hanafuda自动工具 🚀           ║')}
-${chalk.yellow('║  👤    脚本编写：@qklxsqf              ║')}
-${chalk.yellow('║  📢  电报频道：https://t.me/ksqxszq    ║')}
-${chalk.yellow('╚════════════════════════════════════════╝')}${chalk.reset('')}
-  `;
-  console.log(banner);
-  printMessage('请确保 tokens.json 和 pvkey.txt 已准备好！', 'info');
+module.exports = function printBanner(chalk, printMessage) {  
+  const banner = ` 
+  ${chalk.yellow('╔════════════════════════════════════════╗')} 
+  ${chalk.yellow('║      🚀  Hanafuda Automation Tool 🚀   ║')} 
+  ${chalk.yellow('║  👤    Script Author: @qklxsqf         ║')} 
+  ${chalk.yellow('║  📢  Telegram Channel: https://t.me/KatayanAirdropGofC  ║')} 
+  ${chalk.yellow('╚════════════════════════════════════════╝')}
+  ${chalk.reset('')}   
+  `;   
+  
+  console.log(banner);   
+  printMessage('Please make sure tokens.json and pvkey.txt are ready!', 'info'); 
 };
